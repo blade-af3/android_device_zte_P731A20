f7b549234f613fdbfe7e3d7e422a913e73c77956
Sergey Karbainov <karbainovs@mail.ru>
2016-05-04 16:35:53 +0800

    Switch to kernel from source

--- device_P731A20.mk
+++ device_P731A20.mk
@@ -8,14 +8,6 @@ $(call inherit-product-if-exists, vendor/zte/P731A20/P731A20-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/zte/P731A20/overlay
 
 LOCAL_PATH := device/zte/P731A20
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
