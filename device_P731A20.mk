d25647020a046f9462e79f2cce3b16ee38852868
Sergey Karbainov <karbainovs@mail.ru>
2016-05-08 18:45:00 +0800

    update

--- device_P731A20.mk
+++ device_P731A20.mk
@@ -9,15 +9,82 @@ DEVICE_PACKAGE_OVERLAYS += device/zte/P731A20/overlay
 
 LOCAL_PATH := device/zte/P731A20
 
+# sprd telephony
+PRODUCT_PACKAGES += \
+	Dialer \
+	Mms
+
+
+# graphics modules
+PRODUCT_PACKAGES += \
+	libGLES_mali.so \
+	libboost.so \
+	mali.ko
+
+# video modules
+PRODUCT_PACKAGES += \
+	libstagefright_sprd_soft_mpeg4dec \
+	libstagefright_sprd_soft_h264dec \
+	libstagefright_sprd_mpeg4dec \
+	libstagefright_sprd_mpeg4enc \
+	libstagefright_sprd_h264dec \
+	libstagefright_sprd_h264enc \
+	libstagefright_sprd_vpxdec \
+	libstagefright_soft_mjpgdec \
+	libstagefright_sprd_mp3dec \
+	libstagefright_soft_imaadpcmdec
+
+# default audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	libaudio-resampler
+
+# sprd HAL modules
+PRODUCT_PACKAGES += \
+	audio.primary.sc8830 \
+	audio_policy.sc8830 \
+	gralloc.sc8830 \
+	camera.sc8830 \
+	camera2.sc8830 \
+	lights.sc8830 \
+	hwcomposer.sc8830 \
+	sprd_gsp.sc8830
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.sc8830:root/fstab.sc8830 \
+    $(LOCAL_PATH)/rootdir/init.board.rc:root/init.board.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.board.rc:root/init.recovery.board.rc \
     $(LOCAL_PATH)/rootdir/init.sc8830.rc:root/init.sc8830.rc \
     $(LOCAL_PATH)/rootdir/init.sc8830.usb.rc:root/init.sc8830.usb.rc \
     $(LOCAL_PATH)/rootdir/init.storage.rc:root/init.storage.rc \
     $(LOCAL_PATH)/rootdir/ueventd.sc8830.rc:root/ueventd.sc8830.rc \
-    
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/audio/tiny_hw.xml:system/etc/tiny_hw.xml \
+	$(LOCAL_PATH)/audio/codec_pga.xml:system/etc/codec_pga.xml \
+	$(LOCAL_PATH)/audio/audio_hw.xml:system/etc/audio_hw.xml \
+	$(LOCAL_PATH)/audio/audio_para:system/etc/audio_para \
+	$(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+	$(LOCAL_PATH)/configs/focaltech_ts.idc:system/usr/idc/focaltech_ts.idc \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/headset-keyboard.kl:system/usr/keylayout/headset-keyboard.kl \
+    $(LOCAL_PATH)/configs/sci-keypad.kl:system/usr/keylayout/sci-keypad.kl \
+    $(LOCAL_PATH)/configs/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 $(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_AAPT_CONFIG := hdpi
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_P731A20
